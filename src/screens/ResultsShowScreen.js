e9b60b207a2cb24f145e72ae6eb5ca79ccfb7ee4
roycechua23 <royce236@gmail.com>
2020-07-14 16:36:41 +0800

    Add code to render the images of the specific business.

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,12 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList, Image } from 'react-native';
 import yelp from '../api/yelp';
 
 const ResultsShowScreen = ({ navigation }) => {
     const [result, setResult] = useState(null);
     const id = navigation.getParam('id');
-
-    console.log(result);
 
     const getResult = async (id) => {
         const response = await yelp.get(`/${id}`);
@@ -17,11 +15,27 @@ const ResultsShowScreen = ({ navigation }) => {
         getResult(id);
     }, [])
 
+    if (!result) {
+        return null;
+    }
+
     return <View>
-        <Text>Results Show</Text>
+        <Text>{result.name}</Text>
+        <FlatList 
+            data={result.photos}
+            keyExtractor={(photo) => photo}
+            renderItem={({ item }) => {
+                <Image style={styles.image} source={{ uri:'' }} />
+            }}
+        />
     </View>;
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    image: {
+        height: 200, 
+        width: 300,
+    }
+});
 
 export default ResultsShowScreen;
